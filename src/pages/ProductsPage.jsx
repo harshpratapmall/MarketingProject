0adb3adf42d183a183b22b7ac632c07d1b5f1630
satyam kesharwani <satyam.kesharwani@indiamart.com>
2024-06-27 19:14:51 +0530

    Adding Products page

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import styles from './ProductsPage.module.css'
+
+// Contact Page Component
+function ProductsPage() {
+  
+
+  return (
+    <h1>Product Page</h1>
+  );
+}
+
+export default ProductsPage;
